48cdd7e3628a5ecaf54f5885608be8baa263b730
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-01-20 07:37:45 +0530

    Intents Added

---
+++ app/src/main/java/com/mohdeva/learn/tasker/Main.java
@@ -0,0 +1,110 @@
+package com.mohdeva.learn.tasker;
+
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import java.util.Calendar;
+
+public class Main extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDatePicker, btnTimePicker,btnloc,btncont;
+    private EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private String nameString;
+    private TextView int_name;
+    private Intent loc,cont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //Recieve data from intent
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                nameString= null;
+            } else {
+                nameString= extras.getString("Data");
+            }
+        } else {
+            nameString= (String) savedInstanceState.getSerializable("Data");
+        }
+        int_name= (TextView) findViewById(R.id.IntentName);
+        int_name.setText(nameString);
+
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtDate=(EditText)findViewById(R.id.in_date);
+        txtTime=(EditText)findViewById(R.id.in_time);
+        btnloc=(Button)findViewById(R.id.btn_loc);
+        btncont=(Button)findViewById(R.id.btn_call);
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
+
+        btncont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cont = new Intent(Main.this, Cont.class);
+                cont.putExtra("DataCont", nameString);
+                startActivity(cont);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
+}
